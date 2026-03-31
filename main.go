dcf2def6eec0d1d5aa821d3542901184cae5e45d
richardtjustke <rj.justke@gmail.com>
2026-03-31 19:01:16 -0300

    switch

--- main.go
+++ main.go
@@ -1,11 +1,35 @@
 package main
 
-import(
-	"fmt" //formatador
-	"bufio" //leitura de imput 
-	"os"//sistema de arquivos
-)
+import "fmt"
 
-func main(){
-	
+// leitura de imput
+// sistema de arquivos
+type notes struct {
+	ID      int
+	Title   string
+	Content string
+}
+
+var notes []Note
+
+func main() {
+	for {
+		fmt.Println("\n1 - Criar nota")
+		fmt.Println("2 - Listar notas")
+		fmt.Println("3 - Sair")
+
+		var choice int
+		fmt.Scan(&choice)
+
+		switch choice {
+		case 1:
+			createNote()
+		case 2:
+			listNotes()
+		case 3:
+			saveNotes()
+			return
+		}
+
+	}
 }
